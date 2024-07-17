8269c40987bada460c53ac62a59a5540800598f8
Rikako921 <rico.kyg@icloud.com>
2024-07-17 12:40:50 +0900

    車一覧の表示

--- app/javascript/swipe.js
+++ app/javascript/swipe.js
@@ -1,1 +1,15 @@
-const swiper = new Swiper(".swiper", {});
+document.addEventListener("turbo:load", function() {
+  const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+      delay: 5000,
+    }
+  });
+});
